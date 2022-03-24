32c8c22b3eaa99d58379ccd9c0b56191ff48d584
Denilson De Sousa Soares <jhondoe@exemple.com>
2022-03-24 08:58:37 -0300

    Atualização de form

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const txtPeso = document.getElementById("txtPeso");
 const txtAltura = document.getElementById("txtAltura");
 const btnCalculaIMC = document.getElementById("btnCalculaIMC");
 const spnValor = document.getElementById("spnValor");
-const btnEnviaForm = document.getElementById('enviarForm');
+const input = document.getElementById("valida-cpf");
 
 function calcularValor(peso, altura) {
 
@@ -28,8 +28,8 @@ function btnCalculaIMCClick() {
     if (validaCampos()) {
         const peso = Number(txtPeso.value);
         const altura = Number(txtAltura.value);
-        txtPeso.value="";
-        txtAltura.value="";
+        txtPeso.value = "";
+        txtAltura.value = "";
         calcularValor(peso, altura);
     } else {
         alert("Você não colocou valor em um dos campos obrigatórios ou valor é inválido.");
@@ -49,48 +49,51 @@ function validaCampos() {
     return true;
 }
 
-function enviarForm(){
-    var cpf = document.getElementById("valida-cpf");
-    if(!validarCPF(cpf)){
-        alert("dida feio")
+function enviarFormTest() {
+    var cpf = document.getElementById("valida-cpf").value;
+    if (!validarCPF(cpf)) {
+        alert("CPF Inválido ou campo vázio");
+        input.focus();
+        input.classList.add('error');
     }
 }
 
-function validarCPF(cpf) {	
-	cpf = cpf.replace(/[^\d]+/g,'');	
-	if(cpf == '') return false;	
-	// Elimina CPFs invalidos conhecidos	
-	if (cpf.length != 11 || 
-		cpf == "00000000000" || 
-		cpf == "11111111111" || 
-		cpf == "22222222222" || 
-		cpf == "33333333333" || 
-		cpf == "44444444444" || 
-		cpf == "55555555555" || 
-		cpf == "66666666666" || 
-		cpf == "77777777777" || 
-		cpf == "88888888888" || 
-		cpf == "99999999999")
-			return false;		
-	// Valida 1o digito	
-	add = 0;	
-	for (i=0; i < 9; i ++)		
-		add += parseInt(cpf.charAt(i)) * (10 - i);	
-		rev = 11 - (add % 11);	
-		if (rev == 10 || rev == 11)		
-			rev = 0;	
-		if (rev != parseInt(cpf.charAt(9)))		
-			return false;		
-	// Valida 2o digito	
-	add = 0;	
-	for (i = 0; i < 10; i ++)		
-		add += parseInt(cpf.charAt(i)) * (11 - i);	
-	rev = 11 - (add % 11);	
-	if (rev == 10 || rev == 11)	
-		rev = 0;	
-	if (rev != parseInt(cpf.charAt(10)))
-		return false;		
-	return true;   
+
+function validarCPF(cpf) {
+    cpf = cpf.replace(/[^\d]+/g, '');
+    if (cpf == '') return false;
+    // Elimina CPFs invalidos conhecidos	
+    if (cpf.length != 11 ||
+        cpf == "00000000000" ||
+        cpf == "11111111111" ||
+        cpf == "22222222222" ||
+        cpf == "33333333333" ||
+        cpf == "44444444444" ||
+        cpf == "55555555555" ||
+        cpf == "66666666666" ||
+        cpf == "77777777777" ||
+        cpf == "88888888888" ||
+        cpf == "99999999999")
+        return false;
+    // Valida 1o digito	
+    add = 0;
+    for (i = 0; i < 9; i++)
+        add += parseInt(cpf.charAt(i)) * (10 - i);
+    rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11)
+        rev = 0;
+    if (rev != parseInt(cpf.charAt(9)))
+        return false;
+    // Valida 2o digito	
+    add = 0;
+    for (i = 0; i < 10; i++)
+        add += parseInt(cpf.charAt(i)) * (11 - i);
+    rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11)
+        rev = 0;
+    if (rev != parseInt(cpf.charAt(10)))
+        return false;
+    return true;
 }
 
-btnCalculaIMC.addEventListener('click', btnCalculaIMCClick);
+btnCalculaIMC && btnCalculaIMC.addEventListener('click', btnCalculaIMCClick);
